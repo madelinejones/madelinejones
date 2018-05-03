037675cf616534f7851240f0c64d78bc6a323b36
Madeline Jones <brandkast@live.com>
2018-05-03 15:08:53 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var navLinks = document.getElementsByClassName("nav-link");
 
 function changeActive() {
   for (var i = 0; i < navLinks.length; i++) {
-    this.classList.toggle("active");
+    navLinks[i].classList.toggle("active");
   }
 }
 
