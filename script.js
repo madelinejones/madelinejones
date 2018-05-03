b83faf73c575a77bfb8fe6c207550ea225922c12
Madeline Jones <brandkast@live.com>
2018-05-03 15:33:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 function toggleItem(navLink) {
   for (var i = 0; i < navLink.length; i++) {
     navLink[i].addEventListener("click", function() {
-      var current = this;
       for (var i = 0; i < navLink.length; i++) {
-        if (current != navLink[i]) {
+        if (this != navLink[i]) {
           navLink[i].classList.remove('active');
-        } else if (current.classList.contains('active') === true) {
-          current.classList.remove('active');
         } else {
-          current.classList.add('active')
+          this.classList.add('active')
         }
       }
     });
