0a3b0abdf458cf74479469b12c6fcd55447f9c4b
Madeline Jones <brandkast@live.com>
2018-05-03 15:06:10 -0400

    Replaced some code with this keyword

--- script.js
+++ script.js
@@ -3,11 +3,7 @@ var navLinks = document.getElementsByClassName("nav-link");
 
 function changeActive() {
   for (var i = 0; i < navLinks.length; i++) {
-    if (!navLinks[i].classList.contains("active")) {
-      navLinks[i].classList.add("active");
-    } else if (navLinks[i].classList.contains("active")) {
-        navLinks[i].classList.remove("active");
-    }
+    this.classList.toggle("active");
   }
 }
 
