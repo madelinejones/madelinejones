8fb64f772164f0b60b3bb7069cb2dd38cd51af31
Madeline Jones <brandkast@live.com>
2018-05-03 15:36:57 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-function toggleItem(navLink) {
+var navLink = document.querySelectorAll('.nav-link');
+
+function toggleItem() {
   for (var i = 0; i < navLink.length; i++) {
-    navLink[i].addEventListener("click", function() {
-      for (var i = 0; i < navLink.length; i++) {
-        if (this != navLink[i]) {
-          navLink[i].classList.remove('active');
-        } else {
-          this.classList.add('active')
-        }
-      }
-    });
-  };
+    if (this != navLink[i]) {
+      navLink[i].classList.remove('active');
+    } else {
+      this.classList.add('active')
+    }
+  }
 }
-toggleItem(document.querySelectorAll('.nav-link'));
+
+// toggleItem(document.querySelectorAll('.nav-link'));
+
+for (var i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener("click", toggleItem);
+}
